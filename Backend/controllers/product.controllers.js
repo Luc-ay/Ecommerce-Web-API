5175393cbbb6fc7d56c642d3fb8045c011bd72b3
Ayomide <ayomideayomipo147@gmail.com>
2024-12-22 19:38:43 +0100

    Updated Product Route and Controller with CLoudinary

--- Backend/controllers/product.controllers.js
+++ Backend/controllers/product.controllers.js
@@ -1,5 +1,6 @@
 import Redis from 'ioredis'
 import Product from '../models/product.model.js'
+import cloudinary from '../lib/cloudinary.js'
 
 export const getAllProducts = async (req, res) => {
   try {
@@ -36,4 +37,99 @@ export const getFeaturedProducts = async (req, res) => {
   }
 }
 
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, image, category } = req.body
+    if (!name || !description || !price) {
+      res
+        .status(400)
+        .json({ Message: 'name, description & price are required' })
+    }
+
+    let cloudinaryResponse = null
+    if (image) {
+      cloudinaryResponse = await cloudinary.uploader.upload(image, {
+        folder: 'products',
+      })
+    }
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      image: cloudinaryResponse?.secure_url,
+      category,
+    })
+
+    res.status(200).json(product)
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({
+      message: 'Error in Create product API',
+      error: error.message,
+    })
+  }
+}
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id)
+
+    if (!product) {
+      res.status(404).json({ Messgae: 'Product not Found' })
+    }
+
+    if (product.image) {
+      const publicId = product.image.split('/').pop().split('.')[0]
+      try {
+        await cloudinary.uploder.destroy(`products/${publicId}`)
+        console.log('Image Deleted on Cloud')
+      } catch (error) {
+        console.log('Error Deleting Image from Cloudinary', error)
+      }
+    }
+
+    await Product.findByIdAndDelete(req.params.id)
+    res.status(201).json({ Message: 'Product Deleted' })
+  } catch (error) {
+    console.log('Error in Delete Product API', error.message)
+    res
+      .status(500)
+      .json({ Message: 'Error in Delete Product API', Error: error.message })
+  }
+}
+
+export const getRecommendedProducts = async (req, res) => {
+  try {
+    const products = await Product.aggregate([
+      {
+        $sample: { size: 3 },
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          image: 1,
+          price: 1,
+        },
+      },
+    ])
+
+    res.json(products)
+  } catch (error) {
+    console.log(error.message)
+    res
+      .status(500)
+      .json({ Message: 'Error in Recommendation API', Error: error.message })
+  }
+}
+
+// export const getProductsByCategory = async (req, res) => {
+//   try {
+//     const
+//   } catch (error) {
+//     console.log(error.message)
+//     res.status(500).json({Message: "Error in Category API", Error: error.message})
+//   }
+// }
 export const getSingleProduct = async (req, res) => {}
--- Backend/routes/product.routes.js
+++ Backend/routes/product.routes.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import {
+  createProduct,
   getAllProducts,
   getFeaturedProducts,
+  deleteProduct,
+  getRecommendedProducts,
+  getProductsByCategory,
 } from '../controllers/product.controllers.js'
 import { protectRoute, adminRoute } from '../middleware/auth.middleware.js'
 
@@ -9,5 +13,9 @@ const router = express.Router()
 
 router.get('/', protectRoute, adminRoute, getAllProducts)
 router.get('/featured', getFeaturedProducts)
+router.get('/recommendations', getRecommendedProducts)
+router.get('/category/:category', getProductsByCategory)
+router.post('/create', protectRoute, adminRoute, createProduct)
+router.delete('/:id', protectRoute, adminRoute, deleteProduct)
 
 export default router
